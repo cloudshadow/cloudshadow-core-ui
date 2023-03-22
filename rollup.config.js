d0dc7861ef8c90e64fe9247736d63e2f0ea8570d
Cloud He <jingye.he@iherb.com>
2023-03-22 09:48:02 +0800

    chore: update package.json

--- rollup.config.js
+++ rollup.config.js
@@ -3,7 +3,9 @@ import commonjs from '@rollup/plugin-commonjs';
 import typescript from '@rollup/plugin-typescript';
 import terser from '@rollup/plugin-terser';
 import external from 'rollup-plugin-peer-deps-external';
-import postcss from 'rollup-plugin-postcss';
+// import postcss from 'rollup-plugin-postcss';
+import linaria from '@linaria/rollup';
+import css from 'rollup-plugin-css-only';
 
 const packageJson = require('./package.json');
 
@@ -16,18 +18,26 @@ export default {
       sourcemap: true,
       // name: 'react-lib',
     },
-    {
-      file: packageJson.module,
-      format: 'esm',
-      sourcemap: true,
-    },
+    // {
+    //   file: packageJson.module,
+    //   format: 'esm',
+    //   sourcemap: true,
+    // },
   ],
   plugins: [
+    linaria({
+      // exclude: ['node_modules/**'],
+      // evaluate: true,
+      sourceMap: process.env.NODE_ENV !== 'production',
+    }),
+    css({
+      output: 'styles.css',
+    }),
     external(),
     resolve(),
     commonjs(),
     typescript({ tsconfig: './tsconfig.json' }),
-    postcss(),
+    // postcss(),
     terser(),
   ],
 };
